58f8bb707fcc36a634e885eebfb6c193ecbd9e8a
changxiu1 <graham.jimenez95@gmail.com>
2017-02-23 20:29:30 +0800

    notepad

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,100 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $ionicModal, $window, $timeout) {
+
+//initialize modal
+$ionicModal.fromTemplateUrl('my-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+       $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+
+
+  $scope.taskItem = {item:""};
+
+  $scope.items = JSON.parse(window.localStorage.getItem("todoItem")) || [];
+
+
+//save the item
+
+  $scope.saveItem = function(item){
+
+    var a = $window.localStorage.getItem("todoItem") ;
+    a = JSON.parse(a) || [];
+      console.log(a);
+    a.push(item);
+    $window.localStorage.setItem("todoItem", JSON.stringify(a));
+   
+    $timeout(function(){
+        $scope.$apply(function( ){
+        $scope.items = a; 
+         $scope.taskItem = {item:""};
+    });
+
+    })
+  
+    $scope.modal.hide();
+
+  }
+//clear all items
+  $scope.clear = function(){
+
+   
+    $timeout(function(){
+        $scope.$apply(function( ){
+        $scope.items = []; 
+        $window.localStorage.clear();
+    });
+
+    })
+  
+
+
+
+  }
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
